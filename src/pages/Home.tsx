222fe29f3069ff2672d1c3619f593cb202ec9f35
Julia Barbosa <juliadsa@hotmail.com>
2022-02-21 20:13:00 -0300

    criei a tela de login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,54 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
 const Home: React.FC = () => {
   const navigation = useNavigation();
+  const images = {
+    usuario: require('../../assets/usuario.png'),
+    carropolicia: require('../../assets/carro-policia.png'),
+    carroambulancia: require('../../assets/carro-ambulancia.png'),
+    carrobombeiro: require('../../assets/bombeiro-carro.png'),
+  };
 
   return (
-    <View
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}
-    >
-      <Text
-        style={{
-          color: 'black',
-          paddingVertical: 20,
-        }}
-      >
-        Eu sou a Home
-      </Text>
+    <View style={{marginTop: -15}}>  
+      <View style={styles.container}>
+        <Text style={{ color: 'black', paddingVertical: 30, padding: 60 }}> Good Morning{"\n"}
+          <Text style={{ color: 'black', paddingVertical: 20, fontSize: 25, fontWeight: 'bold' }}>Igor Ferráz
+          </Text>
+        </Text>
+        <Image style={styles.homeImageUser} source={images.usuario} />
+      </View>
+      
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate(
+              'Services' as never,
+              {
+                service: 'police',
+              } as never,
+            );
+          }}
 
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('Signup' as never);
-        }}
-        activeOpacity={0.5}
-        style={{
-          paddingHorizontal: 30,
-          paddingVertical: 20,
-          backgroundColor: '#23aeff',
-          borderRadius: 10,
-        }}
-      >
-        <Text style={{color: 'black'}}>Ir para signup</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate(
-            'Services' as never,
-            {
-              service: 'police',
-            } as never,
-          );
-        }}
-        activeOpacity={0.5}
-        style={{
-          paddingHorizontal: 30,
-          paddingVertical: 20,
-          backgroundColor: '#444647',
-          borderRadius: 10,
-        }}
-      >
-        <Text style={{color: 'black'}}>Ir para policias</Text>
-      </TouchableOpacity>
+          activeOpacity={0.5}
+          style={{
+            height: 120,
+            width: 345,
+
+            alignSelf:'center',
+            alignItems: 'center',
+            display: 'flex',
+            backgroundColor: '#C1C1C1',
+            borderRadius: 20,
+          }}
+        >
+          <View style={styles.container}>
+          <Image style={styles.ImagePolice} source={images.carropolicia} />
+          <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 30, padding: 30,}}>Polícia</Text>
+          </View>
+        </TouchableOpacity>
+        {"\n"}
       <TouchableOpacity
         onPress={() => {
           navigation.navigate(
@@ -62,14 +60,23 @@ const Home: React.FC = () => {
         }}
         activeOpacity={0.5}
         style={{
-          paddingHorizontal: 30,
-          paddingVertical: 20,
-          backgroundColor: '#2723ff',
-          borderRadius: 10,
+          height: 120,
+          width: 345,
+          marginTop: 10,
+
+          alignSelf:'center',
+          alignItems: 'center',
+          display: 'flex',
+          backgroundColor: '#B6DCF1',
+          borderRadius: 20,
         }}
       >
-        <Text style={{color: 'black'}}>Ir para ambulancias</Text>
+        <View style={styles.container}>
+        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 30, padding: 30,}}>Samu</Text>
+        <Image style={styles.ImageAmbulance} source={images.carroambulancia} />
+        </View>
       </TouchableOpacity>
+
       <TouchableOpacity
         onPress={() => {
           navigation.navigate(
@@ -81,16 +88,51 @@ const Home: React.FC = () => {
         }}
         activeOpacity={0.5}
         style={{
-          paddingHorizontal: 30,
-          paddingVertical: 20,
-          backgroundColor: '#ff2323',
-          borderRadius: 10,
+          height: 120,
+          width: 345,
+          marginTop: 10,
+
+          alignSelf:'center',
+          alignItems: 'center',
+          display: 'flex',
+          backgroundColor: '#FDBFBF',
+          borderRadius: 20,
         }}
       >
-        <Text style={{color: 'black'}}>Ir para bombeiros</Text>
+        <View style={styles.container}>
+        <Image style={styles.ImageFireman} source={images.carrobombeiro} />
+        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 30, padding: 30,}}>Bombeiro</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  homeImageUser: {
+    width: 80,
+    height: 80,
+  },
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  ImagePolice: {
+    padding: 60,
+    width: 20,
+    height: 20,
+  },
+  ImageAmbulance: {
+    padding: 50,
+    width: 20,
+    height: 20,
+  },
+  ImageFireman: {
+    padding: 50,
+    width: 20,
+    height: 20,
+  },
+});
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,20 +1,27 @@
-import {CardStyleInterpolators, createStackNavigator} from '@react-navigation/stack';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+//import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 import Services from '../pages/Services';
 import Signup from '../pages/Signup';
 
 const Stack = createStackNavigator();
+//const Drawer = createDrawerNavigator();
 
 const Routes: React.FC = () => {
+  
   return (
     <Stack.Navigator
       screenOptions={{
         headerTitleAlign: 'center',
         headerStyle: {
-          backgroundColor: 'rgba(0, 52, 107, 1)',
+          backgroundColor: 'f2f2f2',
+          elevation: 0,
+          shadowOpacity: 0,
+          borderBottomWidth: 0,
         },
-        headerTintColor: '#fff',
+        headerTintColor: '#f2f2f2',
         headerTitleStyle: {
           fontFamily: 'Roboto',
           fontWeight: '400',
@@ -22,9 +29,11 @@ const Routes: React.FC = () => {
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
       }}
     >
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="Services" component={Services} />
+      <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Services" component={Services} />
+ 
     </Stack.Navigator>
   );
 };
